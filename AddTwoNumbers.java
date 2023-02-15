f257de5e1c7161c060c604718c671d0bd0233379
Chris Clark <7968730+cclark31415@users.noreply.github.com>
2023-02-15 12:54:29 -0500

    Create AddTwoNumbers.java
    Adding AddTwoNumbers solution

---
+++ AddTwoNumbers.java
@@ -0,0 +1,144 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class AddTwoNumbers {
+    public static void main(String[] args){
+        AddTwoNumbers solution = new AddTwoNumbers();
+        int[] l1 = { 2, 4, 9};
+        int[] l2 = { 5, 6, 4, 9};
+        ListNode l1Node = solution.createListNode(l1);
+        ListNode l2Node = solution.createListNode(l2);
+        ListNode result = solution.addTwoNumbers(l1Node, l2Node);
+        System.out.println(result.toString());
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode();
+        List<Integer> sumArray = new ArrayList<>();
+        while(l1.next != null || l2.next != null){
+            if(l1.next == null){
+                if (l2.val > 9) {
+                    l2.val = l2.val % 10;
+                    if (l2.next == null) {
+                        l2.next = new ListNode();
+                    }
+                    l2.next.val += 1;
+                }
+                int sum = l1.val + l2.val;
+                
+                if (sum > 9) {
+                    sum = sum % 10;
+                    if (l2.next == null) {
+                        l2.next = new ListNode();
+                    }
+                    l2.next.val += 1;
+                }
+
+                l1.val = 0;
+                sumArray.add(sum);
+                l2 = l2.next;
+            } else if(l2.next == null){
+                if (l1.val > 9) {
+                    l1.val = l1.val % 10;
+                    if(l1.next == null){
+                        l1.next = new ListNode();
+                    }
+                    l1.next.val += 1;
+                }
+                int sum = l1.val + l2.val;
+
+                if (sum > 9) {
+                    sum = sum % 10;
+                    if (l1.next == null) {
+                        l1.next = new ListNode();
+                    }
+                    l1.next.val += 1;
+                }
+
+                l2.val = 0;
+                sumArray.add(sum);
+                l1 = l1.next;
+            } else {
+                int sum = l1.val + l2.val;
+                if(sum > 9){
+                    sum = sum % 10;
+                    if (l1.next == null) {
+                        l1.next = new ListNode();
+                    }
+                    l1.next.val += 1;
+                }
+                sumArray.add(sum);
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+        }
+        if(l1.next == null && l2.next == null){            
+            int sum = l1.val + l2.val;
+            if (sum > 9) {
+                sum = sum % 10;
+                sumArray.add(sum);
+                sumArray.add(1);
+            } else {
+                sumArray.add(sum);
+            }
+        }
+        int[] resultArray = new int[sumArray.size()];
+        for(int r = 0; r < sumArray.size(); r++){
+            resultArray[r] = sumArray.get(r);
+        }
+        result = createListNode(resultArray);
+        return result;
+    }
+    
+    public ListNode createListNode(int[] nums){
+        ListNode result = new ListNode();
+        ListNode thisListNode = result;
+
+        for(int n = 0; n < nums.length; n++){ 
+            thisListNode.val = nums[n];
+            if(n < nums.length - 1){
+                thisListNode.next = new ListNode();
+                thisListNode = thisListNode.next;
+            }
+        }
+        return result;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+
+        }
+
+        ListNode(int val) { 
+            this.val = val; 
+        }
+
+        ListNode(int val, ListNode next) { 
+            this.val = val; 
+            this.next = next; 
+        }
+        
+        public String toString(ListNode node) {
+            StringBuilder result = new StringBuilder();
+            while (node.next != null) {
+                result.append(node.val);
+            }
+            if (node.next == null) {
+                result.append(node.val);
+            }
+            return result.toString();
+        }
+    }
+}
