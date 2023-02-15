634d972d8e017f45efc4e944e1e70bbe24a16f69
Chris Clark <7968730+cclark31415@users.noreply.github.com>
2023-02-15 12:55:47 -0500

    Update AddTwoNumbers.java
    updated description in the comment

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -2,6 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order, and each of their nodes
+ * contains a single digit. Add the two numbers and return the sum as a linked
+ * list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * Constraints:
+ * 
+ * The number of nodes in each linked list is in the range [1, 100].
+ * 0 <= Node.val <= 9
+ * It is guaranteed that the list represents a number that does not have leading
+ * zeros.
+ * 
+ * 
+ * 
+ * 
  * Definition for singly-linked list.
  * public class ListNode {
  * int val;
@@ -10,6 +28,9 @@ import java.util.List;
  * ListNode(int val) { this.val = val; }
  * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
+ * 
+ * 
+ * https://leetcode.com/problems/add-two-numbers/description/
  */
 public class AddTwoNumbers {
     public static void main(String[] args){
